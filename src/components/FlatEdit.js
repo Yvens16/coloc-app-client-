77fe312c4fd56d7a16e0647c714142253c25d203
Nerimene Akari <akari.nerimene@gmail.com>
2018-10-05 13:48:32 +0200

    css style

--- src/components/FlatEdit.js
+++ src/components/FlatEdit.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import api from "../api";
 import { Redirect, Link } from "react-router-dom";
+import { Container, Row, Col, Input, Button } from "mdbreact";
 
 class FlatEdit extends Component {
   constructor(props) {
@@ -120,7 +121,250 @@ class FlatEdit extends Component {
       <section>
         <div>
           <h3>Edit my flat</h3>
+
           <form onSubmit={event => this.handleSubmit(event)}>
+            {/* <!-- Grid row --> */}
+            <div class="form-row">
+              {/* <!-- Grid column --> */}
+              <div class="col-md-6">
+                {/* <!-- Material input --> */}
+                <div class="md-form form-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="inputEmail4MD"
+                    value={streetNum}
+                    name="streetNum"
+                    onChange={event => this.updateInput(event)}
+                    placeholder="40"
+                  />
+                  <label for="inputEmail4MD">Street Number</label>
+                </div>
+              </div>
+              {/* <!-- Grid column --> */}
+
+              {/* <!-- Grid column --> */}
+              <div class="col-md-6">
+                {/* <!-- Material input --> */}
+                <div class="md-form form-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="inputPassword4MD"
+                    value={zipCode}
+                    name="zipCode"
+                    onChange={event => this.updateInput(event)}
+                    placeholder="75008"
+                  />
+                  <label for="inputPassword4MD">Zip Code</label>
+                </div>
+              </div>
+              {/* <!-- Grid column --> */}
+            </div>
+            {/* <!-- Grid row --> */}
+
+            {/* <!-- Grid row --> */}
+            <div class="row">
+              {/* <!-- Grid column --> */}
+              <div class="col-md-12">
+                {/* <!-- Material input --> */}
+                <div class="md-form form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="inputAddressMD"
+                    value={address}
+                    name="address"
+                    onChange={event => this.updateInput(event)}
+                    placeholder="rue du Colisée"
+                  />
+                  <label for="inputAddressMD">Address</label>
+                </div>
+              </div>
+
+              {/* <!-- Grid column --> */}
+
+              {/* <!-- Grid column --> */}
+            </div>
+            {/* <!-- Grid row --> */}
+
+            {/* <!-- Gri/d row --> */}
+            <div class="form-row">
+              {/* <!-- Grid column --> */}
+              <div class="col-md-6">
+                {/* <!-- Material input --> */}
+                <div class="md-form form-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="inputCityMD"
+                    name="area"
+                    value={area}
+                    onChange={event => this.updateInput(event)}
+                    placeholder="90"
+                  />
+                  <label for="inputCityMD">Area (m²)</label>
+                </div>
+              </div>
+              {/* <!-- Grid column --> */}
+
+              {/* <!-- Grid column --> */}
+              <div class="col-md-6">
+                {/* <!-- Material input --> */}
+                <div class="md-form form-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="inputZipMD"
+                    value={rent}
+                    name="rent"
+                    onChange={event => this.updateInput(event)}
+                    placeholder="600"
+                  />
+                  <label for="inputZipMD">Rent (€)</label>
+                </div>
+              </div>
+              {/* <!-- Grid column --> */}
+            </div>
+
+            <div class="form-row">
+              <div class="col-md-4">
+                <label>Roommates present:</label>
+                <select
+                  class="browser-default"
+                  value={roomMate}
+                  name="roomMate"
+                  onChange={event => this.updateInput(event)}
+                >
+                  <option disabled selected>
+                    Choose your option
+                  </option>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10+</option>
+                </select>
+              </div>
+
+              <div class="col-md-4">
+                <label>Number of room:</label>
+                <select
+                  class="browser-default"
+                  value={roomNum}
+                  name="roomNum"
+                  onChange={event => this.updateInput(event)}
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10+</option>
+                </select>
+              </div>
+
+              <div class="col-md-4">
+                <label>Housing:</label>
+                <select
+                  class="browser-default"
+                  value={housing}
+                  name="housing"
+                  onChange={event => this.updateInput(event)}
+                >
+                  <option value="Studio">Studio</option>
+                  <option value="Appartment">Appartment</option>
+                  <option value="Duplex">Duplex</option>
+                  <option value="Loft">Loft</option>
+                  <option value="House">House</option>
+                  <option value="Boat">Boat</option>
+                </select>
+              </div>
+            </div>
+
+            {/* <!--Textarea with icon prefix--> */}
+            <div class="md-form amber-textarea active-amber-textarea-2">
+              <i class="fa fa-pencil prefix" />
+              <textarea
+                type="text"
+                id="form24"
+                class="md-textarea form-control"
+                rows="3"
+                name="description"
+                value={description}
+                onChange={event => this.updateInput(event)}
+              />
+              <label for="form24">
+                Describe your environnement, how's the appartment? how it is to
+                live with you? etc...
+              </label>
+            </div>
+
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroupFileAddon01">
+                  Picture(s)
+                </span>
+              </div>
+              <div class="custom-file">
+                <input
+                  type="file"
+                  class="custom-file-input"
+                  id="inputGroupFile01"
+                  onChange={event => this.multipleUpload(event)}
+                  multiple
+                  name="picture"
+                  aria-describedby="inputGroupFileAddon01"
+                />
+                <label class="custom-file-label" for="inputGroupFile01">
+                  Choose file
+                </label>
+              </div>
+            </div>
+
+            {/* <!-- Card group --></div> */}
+            <div class="card-group">
+              {/* <!-- Card --> */}
+              {picture.map((onePic, index) => {
+                return (
+                  <div key={index} class="card mb-4">
+                    {/* <!-- Card image --> */}
+                    <div class="view overlay">
+                      <img
+                        class="card-img-top"
+                        src={onePic}
+                        alt="Card image cap"
+                      />
+                      <a href="#!">
+                        <div class="mask rgba-white-slight" />
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            {/* <!-- Card group --> */}
+            {/* <!-- Grid row --> */}
+
+            <button type="submit" class="btn btn-primary btn-md">
+              Save changes
+            </button>
+          </form>
+          <Link to={`/flats/${_id}`}>
+            <Button>Cancel</Button>
+          </Link>
+          {/* --------------------------------------------------------------- */}
+          {/* <form onSubmit={event => this.handleSubmit(event)}>
             <label>
               Street Number:{" "}
               <input
@@ -258,7 +502,7 @@ class FlatEdit extends Component {
 
             <button>Saves changes</button>
             <Link to={`/flats/${_id}`}>Cancel</Link>
-          </form>
+          </form> */}
         </div>
       </section>
     );
--- src/components/WhosLike.js
+++ src/components/WhosLike.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import api from "../api";
 import { Link } from "react-router-dom";
+import {
+  Card,
+  CardImage,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardGroup,
+  Button
+} from "mdbreact";
 
 class WhosLike extends Component {
   constructor(props) {
@@ -54,8 +63,36 @@ class WhosLike extends Component {
 
     return (
       <section>
-        <h2>Who's like my flat?</h2>
-        {likes.map(oneLike => (
+        <h2>Who likes my flat?</h2>
+        <CardGroup>
+          {likes.map(oneLike => {
+            return (
+              <Card key={oneLike._id}>
+                <CardImage
+                  src={oneLike.avatar}
+                  alt="Card image cap"
+                  top
+                  hover
+                  overlay="white-slight"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    {oneLike.firstName} {oneLike.lastName}
+                  </CardTitle>
+                  <CardText tag="h5">
+                    {oneLike.age} years | {oneLike.job}
+                  </CardText>
+
+                  <CardText>{oneLike.presentation}</CardText>
+                  <CardText>Phone: {oneLike.phone}</CardText>
+                  <CardText>Email: {oneLike.email}</CardText>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </CardGroup>
+
+        {/* {likes.map(oneLike => (
           <div key={oneLike._id}>
             <img src={oneLike.avatar} />
             <h3>
@@ -72,7 +109,7 @@ class WhosLike extends Component {
               Budget: <b>{oneLike.budget} €</b>
             </p>
           </div>
-        ))}
+        ))} */}
 
         {/* <Link to={`/flats/${}`}>Back to details</Link> */}
       </section>
